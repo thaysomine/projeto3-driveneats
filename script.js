ba19893df0f0ba4f49363e759409c8c1c0d26e15
Thays Omine <thaysomine@gmail.com>
2022-01-29 22:22:45 -0300

    variaveis globais adicionadas

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-let pratoEscolhido = 0;
-let bebidaEscolhida = 0;
-let sobremesaEscolhida = 0;
+let pratoEscolhido = null;
+let bebidaEscolhida = null;
+let sobremesaEscolhida = null;
 
-function selecionarPratos(classeBotao) {
+function selecionarPratos(classeBotao,tipoPrato) {
     const selecionado = document.querySelector(".pratos .selecionado");
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
     }
 
+    pratoEscolhido = tipoPrato;
+
     const botao = document.querySelector(classeBotao);
     botao.classList.add("selecionado");
-
 }
 
-function selecionarBebidas(classeBotao) {
+function selecionarBebidas(classeBotao,tipoPrato) {
     const selecionado = document.querySelector(".bebidas .selecionado");
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
     }
     
+    bebidaEscolhida = tipoPrato;
+
     const botao = document.querySelector(classeBotao);
     botao.classList.add("selecionado");
-
 }
 
-function selecionarSobremesas(classeBotao) {
+function selecionarSobremesas(classeBotao,tipoPrato) {
     const selecionado = document.querySelector(".sobremesas .selecionado");
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
     }
     
+    sobremesaEscolhida = tipoPrato;
+
     const botao = document.querySelector(classeBotao);
     botao.classList.add("selecionado");
-
 }
 
