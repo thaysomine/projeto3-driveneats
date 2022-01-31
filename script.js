0dfcbc89436dfe6e72594e0645e9b37f5456e130
Thays Omine <thaysomine@gmail.com>
2022-01-31 14:35:17 -0300

    Versão final com um dos bônus

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let pratoEscolhido = null;
 let bebidaEscolhida = null;
 let sobremesaEscolhida = null;
 
-let precoTotal;
-
 let precoPrato;
 let precoBebida;
 let precoSobremesa;
+
+let precoTotal;
 
 let nomePrato;
 let nomeBebida;
 let nomeSobremesa;
 
-// Converter
+// Converter 
 function converte() {
     precoPrato = pratoEscolhido.getElementsByTagName("p")[2].innerText;
     precoBebida = bebidaEscolhida.getElementsByTagName("p")[2].innerText;
@@ -26,7 +26,7 @@ function converte() {
     nomeSobremesa = sobremesaEscolhida.getElementsByTagName("p")[0].innerText;
 }
 
-// Libera nosso botão
+// Libera botão de finalizar pedido
 function liberarBotao() {
     if (pratoEscolhido !== null && bebidaEscolhida !== null && sobremesaEscolhida !== null) {
         document.querySelectorAll(".finalizar-pedido")[0].classList.add("oculto");
@@ -45,6 +45,7 @@ function selecionarPratos(prato) {
     pratoEscolhido = prato;
     liberarBotao();
 }
+// Seleciona a bebida
 function selecionarBebidas(bebida) {
     if (bebidaEscolhida !== null) {
         bebidaEscolhida.classList.remove("selecionado");
@@ -56,7 +57,7 @@ function selecionarBebidas(bebida) {
     bebidaEscolhida = bebida;
     liberarBotao();
 }
-
+// Seleciona a sobremesa 
 function selecionarSobremesas(sobremesa) {
     if (sobremesaEscolhida !== null) {
         sobremesaEscolhida.classList.remove("selecionado");
@@ -68,13 +69,18 @@ function selecionarSobremesas(sobremesa) {
     liberarBotao();
 }
 
+// mensagem de finalizar pedido
 function finalizarPedido() {
     converte();
 
-    let mensagem = `Olá gostaria de fazer o pedido:%0a- Prato: ${nomePrato}%0a- Bebida: ${nomeBebida}%0a- Sobremesa: ${nomeSobremesa}%0aPreço: R$${precoTotal}`;
+    let nome = prompt('Nome:');
+    let endereco = prompt('Endereço para entrega:')
+
+    let mensagem = `Olá gostaria de fazer o pedido:%0a- Prato: ${nomePrato}%0a- Bebida: ${nomeBebida}%0a- Sobremesa: ${nomeSobremesa}%0aPreço: R$${precoTotal}%0a%0a Nome: ${nome}%0a Endereço: ${endereco}`;
     window.open("https://wa.me/+5547992705975?text=" + mensagem);
 }
 
+// seleciona apenas os numeros
 function apenasNumeros(string) 
 {
     let numsStr = string.replace(/[^0-9]/g,'')
